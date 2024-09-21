b25c014e2f799bb1536502174e0a6dbd5072563f
spriveen <rpeanuka@gmail.com>
2024-09-21 11:18:08 -0700

    update.hero.tsx

--- my-app/components/Hero.tsx
+++ my-app/components/Hero.tsx
@@ -5,7 +5,7 @@ import Button from './Button'
 const Hero = () => {
   return (
     <section className='max-container padding-container flex flex-col 
-      gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row '>
+      gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       
       {/* Hero Map Section */}
       <div className='hero-map' />
@@ -44,11 +44,11 @@ const Hero = () => {
 
         {/* Buttons Section */}
         <div className='flex flex-col w-full gap-3 sm:flex-row'>
-       <Button 
-        type='button'
-        title='Download App'
-        variant='btn_green'
-        />
+          <Button 
+            type='button'
+            title='Download App'
+            variant='btn_green'
+          />
           <Button 
             type='button'
             title='How we work?'
@@ -58,31 +58,36 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className='relative flex flex-1 items-start 
-      '>
-       <div className='relative z-20 w-[268px] flex-col gap-8
-       rounded-3xl bg-green-90 px-7 py-8'>
-       <div className='flex flex-col'>
-       <div className='flexBetween'>
-        <p className='regular-16 text-gray-20'> Location</p>
-        <Image src="/close.svg" alt='close' width={24}
-         height={24} />
-        
-       </div>
-       <p className='bold-20 text-white'>Aguas Calientes</p>
-
-       <div className='flexBetween'>
-        <div className='flex flex-col'>
-          <p className='regular-16 block text-gray-20'>Distance</p>
-          <p className='bold-20 text-white'>173.28 mi</p>
+      {/* RIGHT Section */}
+      <div className='relative flex flex-1 items-start'>
+        <div className='relative z-20 flex w-[268px] flex-col gap-8
+          rounded-3xl bg-green-90 px-7 py-8'>
 
 
+          <div className='flex flex-col'>
+            <div className='flex justify-between'>
+              <p className='regular-16 text-gray-20'>Location</p>
+              <Image src="/close.svg" alt='close' width={24} height={24} />
+            </div>
+            <p className='bold-20 text-white'>Aguas Calientes</p>
+          </div>
+
+
+
+          <div className='flex justify-between'>
+            <div className='flex flex-col'>
+              <p className='regular-16 block text-gray-20'>Distance</p>
+              <p className='bold-20 text-white'>173.28 mi</p>
+            </div>
+            <div className='flex flex-col'>
+              <p className='regular-16 block text-gray-20'>Elevation</p>
+              <p className='bold-20 text-white'>2.040km</p>
+            </div>
+          </div>
         </div>
-       </div>
-       </div>
-       </div>
       </div>
     </section>
   )
 }
+
 export default Hero
