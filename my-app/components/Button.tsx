9d63d19907d772476f8dbc20be7f9326eb741689
spriveen <rpeanuka@gmail.com>
2024-09-22 00:02:54 -0700

    update Get app.tsx

--- my-app/components/Button.tsx
+++ my-app/components/Button.tsx
@@ -11,13 +11,14 @@ type ButtonProps = {
 const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
+      
+      className={`flexCenter  gap-3 rounded-full border ${variant }
+          ${full && 'w-full'}`}
       type={type}
-      className={`flex items-center justify-center gap-2 rounded-full ${
-        variant === 'btn_dark_green' ? 'bg-green-90 text-white' : ''
-      } ${full ? 'w-full' : ''}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className='bold-16 whitespace-nowrap'>{title}</label>
+      <label className='bold-16 whitespace-nowrap cursor-pointer'>
+        {title}</label>
     </button>
   );
 };
